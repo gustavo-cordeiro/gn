123030604aa41bc39474d9d5720ef418bf060825
Gustavo Cordeiro <oliv.dg@gmail.com>
2016-07-29 19:44:11 -0300

    Firts logic withoud feedback validation

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -1,12 +1,110 @@
-// Main javascript entry point
-// Should handle bootstrapping/starting application
-
 'use strict';
-
 import $ from 'jquery';
-import Link from '../_modules/link/link';
+require('babel-polyfill');
+
+function _getRequiredInputName($elements) {
+  const allNames = $elements.map( (index,el) => el.getAttribute('name'));
+
+  return [... new Set(allNames)];
+}
+
+function _validate($element, names) {
+  const values = names.map(name => {
+    const $el = $element.find(`[name='${name}']`);
+    let val = undefined;
+
+    if($el.get(1)) {
+      val = $el.filter(':checked').val() || '';
+    }
+    else {
+      val = $el.val();
+    }
+
+    return val;
+  });
+
+  return values.find(val => val === '') === undefined;
+}
+
+function _setCurrentBreadcrumb($elemnt, index) {
+  const $current = $($elemnt.find('.breadcrumb__item').get(index));
+
+  $elemnt.find('.is-active').removeClass('is-active');
+
+  $current.addClass('is-active');
+
+  return $current.addClass('is-active');
+}
+
+class NinjaForm {
+  constructor($form) {
+    this.$form = $form;
+    this.currentStep = undefined;
+    this.$user = $form.find('#user');
+    this.$request = $form.find('#request');
+    this.$breadcrumb = $form.find('.breadcrumb');
+
+    this.requiredUserInputsNames = _getRequiredInputName(this.$user.find('[data-required]'));
+    this.requiredRequestInputsNames = _getRequiredInputName(this.$request.find('[data-required]'));
+
+    this.init();
+  }
+
+  init() {
+    this.setStep(0);
+
+    this.$request
+      .find('.submit')
+      .on('click', event => this.submitRequest());
+
+    this.$user
+      .find('.submit')
+      .on('click', event => {
+        event.preventDefault();
+        this.submitUser();
+
+        return false;
+      });
+  }
+
+  submitRequest() {
+    if(this.validateRequiredRequestInputs()) {
+      this.setStep(1);
+    }
+  }
+
+  submitUser() {
+    if(this.validateUserRequestInputs()) {
+      window.location = `${window.location.href}images/quem-e-esse-pokemon.jpg`;
+    }
+  }
+
+  setStep(index) {
+    const $currentBreadcrumb = _setCurrentBreadcrumb(this.$breadcrumb, index);
+    this.currentStep = $currentBreadcrumb.data('refer');
+
+    if(this.currentStep === '#request') {
+      this.$user.addClass('is-hide');
+      this.$request.removeClass('is-hide');
+    }
+    else {
+      this.$request.addClass('is-hide');
+      this.$user.removeClass('is-hide');
+    }
+  }
+
+  validateRequiredRequestInputs() {
+    return _validate(this.$request, this.requiredRequestInputsNames);
+  }
+
+  validateUserRequestInputs() {
+    return _validate(this.$user, this.requiredUserInputsNames);
+  }
+
+}
 
 $(() => {
-  new Link(); // Activate Link modules logic
-  console.log('Welcome to Yeogurt!');
+  new NinjaForm($('.form'));
 });
+
+
